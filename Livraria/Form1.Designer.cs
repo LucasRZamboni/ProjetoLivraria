5a4b3e142d08ce81d6a465fd53ffb93f67298e6b
unknown <lucasrzamboni@hotmail.com>
2022-09-30 21:33:19 -0300

    Primeiro commit

---
+++ Livraria/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace Livraria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_nomeLivro = new System.Windows.Forms.TextBox();
+            this.cB_categoria = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_valorPagar = new System.Windows.Forms.TextBox();
+            this.tb_qtdVendida = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Tempus Sans ITC", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(15, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome do Livro:";
+            // 
+            // tb_nomeLivro
+            // 
+            this.tb_nomeLivro.Location = new System.Drawing.Point(112, 93);
+            this.tb_nomeLivro.Name = "tb_nomeLivro";
+            this.tb_nomeLivro.Size = new System.Drawing.Size(137, 20);
+            this.tb_nomeLivro.TabIndex = 2;
+            this.tb_nomeLivro.TextChanged += new System.EventHandler(this.button2_Click);
+            // 
+            // cB_categoria
+            // 
+            this.cB_categoria.FormattingEnabled = true;
+            this.cB_categoria.Items.AddRange(new object[] {
+            "Romance",
+            "Aventura",
+            "Suspense",
+            "Drama",
+            "Terror"});
+            this.cB_categoria.Location = new System.Drawing.Point(112, 145);
+            this.cB_categoria.Name = "cB_categoria";
+            this.cB_categoria.Size = new System.Drawing.Size(100, 21);
+            this.cB_categoria.TabIndex = 3;
+            this.cB_categoria.SelectedIndexChanged += new System.EventHandler(this.cB_categoria_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Red;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label2.Font = new System.Drawing.Font("Tempus Sans ITC", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(45, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Categoria:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Red;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label3.Font = new System.Drawing.Font("Tempus Sans ITC", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(284, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Valor a pagar:";
+            // 
+            // tb_valorPagar
+            // 
+            this.tb_valorPagar.Location = new System.Drawing.Point(372, 146);
+            this.tb_valorPagar.Name = "tb_valorPagar";
+            this.tb_valorPagar.ReadOnly = true;
+            this.tb_valorPagar.Size = new System.Drawing.Size(100, 20);
+            this.tb_valorPagar.TabIndex = 6;
+            // 
+            // tb_qtdVendida
+            // 
+            this.tb_qtdVendida.Location = new System.Drawing.Point(372, 93);
+            this.tb_qtdVendida.Name = "tb_qtdVendida";
+            this.tb_qtdVendida.Size = new System.Drawing.Size(51, 20);
+            this.tb_qtdVendida.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Red;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label4.Font = new System.Drawing.Font("Tempus Sans ITC", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(288, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Qtd Vendida:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Font = new System.Drawing.Font("Tempus Sans ITC", 8.25F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(12, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "&Limpar dados";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Font = new System.Drawing.Font("Tempus Sans ITC", 8.25F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(333, 191);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "&Confirmar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.Font = new System.Drawing.Font("Tempus Sans ITC", 8.25F, System.Drawing.FontStyle.Bold);
+            this.button3.Location = new System.Drawing.Point(414, 191);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Calcular &valor";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Tempus Sans ITC", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label5.Location = new System.Drawing.Point(138, 36);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(258, 35);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Promoção de Livros";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.BackgroundImage = global::Livraria.Properties.Resources._01;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button4.Location = new System.Drawing.Point(497, 1);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(23, 23);
+            this.button4.TabIndex = 13;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.BackgroundImage = global::Livraria.Properties.Resources.gray_wall_with_a_row_of_spotlights_in_an_empty_room;
+            this.ClientSize = new System.Drawing.Size(521, 226);
+            this.ControlBox = false;
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tb_qtdVendida);
+            this.Controls.Add(this.tb_valorPagar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cB_categoria);
+            this.Controls.Add(this.tb_nomeLivro);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_nomeLivro;
+        private System.Windows.Forms.ComboBox cB_categoria;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_valorPagar;
+        private System.Windows.Forms.TextBox tb_qtdVendida;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Livraria/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Livraria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tb_nomeLivro.Text))
+            {
+                
+                button3.Visible = false;
+                
+                MessageBox.Show("Nome do livro não preenchido!!", "Atenção!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                tb_nomeLivro.Focus();
+            }
+            else
+            {
+                button3.Visible = true;
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tb_nomeLivro.Text = string.Empty;
+            tb_qtdVendida.Text = string.Empty;
+            tb_valorPagar.Text = string.Empty;
+            cB_categoria.Text = string.Empty;
+            button3.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(tb_nomeLivro.Text))
+            {
+                MessageBox.Show("Nome do livro não preenchido!!", "Atenção!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                float valorPagar, qtdVendida;
+                if (cB_categoria.Text == "Aventura" || cB_categoria.Text == "Romance")
+                {
+                    qtdVendida = int.Parse(tb_qtdVendida.Text);
+                    valorPagar = 20 * qtdVendida;
+                    tb_valorPagar.Text = valorPagar.ToString("C");
+                }
+                else
+                {
+                    qtdVendida = int.Parse(tb_qtdVendida.Text);
+                    valorPagar = 30 * qtdVendida;
+
+                    tb_valorPagar.Text = valorPagar.ToString("C");
+                }
+            }
+                
+        }
+
+        private void cB_categoria_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cB_categoria.Text == "Aventura" || cB_categoria.Text == "Romance")
+            {
+                MessageBox.Show("Categoria em promoção!! R$ 20,00", "Atenção!!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                MessageBox.Show("Preço normal!! R$ 30,00", "Atenção!!", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
